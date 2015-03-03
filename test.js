309b05ae365a8a49accf1ef410f62dcb279babfd
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-03-03 23:36:24 +0100

    test clean up
    * get rid of old `ts` code (base parameter)
    * abstract setTimeout() and db2arr() into verifyIn()
    which is a common test use case
    * pass t.end.bind(t) as cb to final calls to get
    rid of setTimeout() when it isn't really needed

--- test.js
+++ test.js
@@ -53,12 +53,16 @@ function randomPutBatch (length) {
     , randomize = function () {
       return random.string({ enc: 'base58', length: 10 })
     }
-
   for (var i = 0; i < length; ++i) {
     batch.push({ type: 'put', key: randomize(), value: randomize() })
   }
-
   return batch
+}
+
+function verifyIn(delay, createReadStream, t, cb, opts) {
+  setTimeout(function () {
+    db2arr(createReadStream, t, cb, opts)
+  }, delay)
 }
 
 test('single ttl entry', function (t, db) {
@@ -70,28 +74,19 @@ test('single ttl entry', function (t, db) {
 test('single ttl entry with put', function (t, db, createReadStream) {
   db.put('foo', 'foovalue', function (err) {
     t.notOk(err, 'no error')
-    var base = Date.now() // *should* be able to catch it to the ms
     db.put('bar', 'barvalue', { ttl: 100 }, function (err) {
       t.notOk(err, 'no error')
       db2arr(createReadStream, t, function (arr) {
-        var ts = base + 100
-        // allow 1ms leeway
-        if (arr[3] && arr[3].value != String(ts))
-          ts++
-
         contains(t, arr, /!ttl!x!\d{13}!bar/, 'bar')
         contains(t, arr, '!ttl!bar', /\d{13}/)
         contains(t, arr, 'bar', 'barvalue')
         contains(t, arr, 'foo', 'foovalue')
-        setTimeout(function () {
-          db2arr(createReadStream, t, function (arr) {
-            t.deepEqual(arr, [
-                { key: 'foo', value: 'foovalue' }
-            ])
-
-            t.end()
-          })
-        }, 150)
+        verifyIn(150, createReadStream, t, function (arr) {
+          t.deepEqual(arr, [
+            { key: 'foo', value: 'foovalue' }
+          ])
+          t.end()
+        })
       })
     })
   })
@@ -99,28 +94,26 @@ test('single ttl entry with put', function (t, db, createReadStream) {
 
 test('multiple ttl entries with put', function (t, db, createReadStream) {
   var expect = function (delay, keys, cb) {
-        setTimeout(function () {
-          db2arr(createReadStream, t, function (arr) {
-            t.equal(arr.length, 1 + keys * 3, 'correct number of entries in db')
-            contains(t, arr, 'afoo', 'foovalue')
-            if (keys >= 1) {
-              contains(t, arr, 'bar1', 'barvalue1')
-              contains(t, arr, /^!ttl!x!\d{13}!bar1$/, 'bar1')
-              contains(t, arr, '!ttl!bar1', /^\d{13}$/)
-            }
-            if (keys >= 2) {
-              contains(t, arr, 'bar2', 'barvalue2')
-              contains(t, arr, /^!ttl!x!\d{13}!bar2$/, 'bar2')
-              contains(t, arr, '!ttl!bar2', /^\d{13}$/)
-            }
-            if (keys >= 3) {
-              contains(t, arr, 'bar3', 'barvalue3')
-              contains(t, arr, /^!ttl!x!\d{13}!bar3$/, 'bar3')
-              contains(t, arr, '!ttl!bar3', /^\d{13}$/)
-            }
-            cb && cb()
-          })
-        }, delay)
+        verifyIn(delay, createReadStream, t, function (arr) {
+          t.equal(arr.length, 1 + keys * 3, 'correct number of entries in db')
+          contains(t, arr, 'afoo', 'foovalue')
+          if (keys >= 1) {
+            contains(t, arr, 'bar1', 'barvalue1')
+            contains(t, arr, /^!ttl!x!\d{13}!bar1$/, 'bar1')
+            contains(t, arr, '!ttl!bar1', /^\d{13}$/)
+          }
+          if (keys >= 2) {
+            contains(t, arr, 'bar2', 'barvalue2')
+            contains(t, arr, /^!ttl!x!\d{13}!bar2$/, 'bar2')
+            contains(t, arr, '!ttl!bar2', /^\d{13}$/)
+          }
+          if (keys >= 3) {
+            contains(t, arr, 'bar3', 'barvalue3')
+            contains(t, arr, /^!ttl!x!\d{13}!bar3$/, 'bar3')
+            contains(t, arr, '!ttl!bar3', /^\d{13}$/)
+          }
+          cb && cb()
+        })
       }
 
   db.put('afoo', 'foovalue')
@@ -135,33 +128,32 @@ test('multiple ttl entries with put', function (t, db, createReadStream) {
 })
 
 test('multiple ttl entries with batch-put', function (t, db, createReadStream) {
-  var expect = function (delay, keys) {
-        setTimeout(function () {
-          db2arr(createReadStream, t, function (arr) {
-            t.equal(arr.length, 1 + keys * 3, 'correct number of entries in db')
-            contains(t, arr, 'afoo', 'foovalue')
-            if (keys >= 1) {
-              contains(t, arr, 'bar1', 'barvalue1')
-              contains(t, arr, /^!ttl!x!\d{13}!bar1$/, 'bar1')
-              contains(t, arr, '!ttl!bar1', /^\d{13}$/)
-            }
-            if (keys >= 2) {
-              contains(t, arr, 'bar2', 'barvalue2')
-              contains(t, arr, /^!ttl!x!\d{13}!bar2$/, 'bar2')
-              contains(t, arr, '!ttl!bar2', /^\d{13}$/)
-            }
-            if (keys >= 3) {
-              contains(t, arr, 'bar3', 'barvalue3')
-              contains(t, arr, /^!ttl!x!\d{13}!bar3$/, 'bar3')
-              contains(t, arr, '!ttl!bar3', /^\d{13}$/)
-            }
-            if (keys >= 3) {
-              contains(t, arr, 'bar4', 'barvalue4')
-              contains(t, arr, /^!ttl!x!\d{13}!bar4$/, 'bar4')
-              contains(t, arr, '!ttl!bar4', /^\d{13}$/)
-            }
-          })
-        }, delay)
+  var expect = function (delay, keys, cb) {
+        verifyIn(delay, createReadStream, t, function (arr) {
+          t.equal(arr.length, 1 + keys * 3, 'correct number of entries in db')
+          contains(t, arr, 'afoo', 'foovalue')
+          if (keys >= 1) {
+            contains(t, arr, 'bar1', 'barvalue1')
+            contains(t, arr, /^!ttl!x!\d{13}!bar1$/, 'bar1')
+            contains(t, arr, '!ttl!bar1', /^\d{13}$/)
+          }
+          if (keys >= 2) {
+            contains(t, arr, 'bar2', 'barvalue2')
+            contains(t, arr, /^!ttl!x!\d{13}!bar2$/, 'bar2')
+            contains(t, arr, '!ttl!bar2', /^\d{13}$/)
+          }
+          if (keys >= 3) {
+            contains(t, arr, 'bar3', 'barvalue3')
+            contains(t, arr, /^!ttl!x!\d{13}!bar3$/, 'bar3')
+            contains(t, arr, '!ttl!bar3', /^\d{13}$/)
+          }
+          if (keys >= 3) {
+            contains(t, arr, 'bar4', 'barvalue4')
+            contains(t, arr, /^!ttl!x!\d{13}!bar4$/, 'bar4')
+            contains(t, arr, '!ttl!bar4', /^\d{13}$/)
+          }
+          cb && cb()
+        })
       }
 
   db.put('afoo', 'foovalue')
@@ -174,164 +166,97 @@ test('multiple ttl entries with batch-put', function (t, db, createReadStream) {
     , { type: 'put', key: 'bar4', value: 'barvalue4' }
   ], { ttl: 120 })
 
-  expect(20, 4)
-
-  setTimeout(t.end.bind(t), 275)
+  expect(20, 4, t.end.bind(t))
 })
 
 test('prolong entry life with additional put', function (t, db, createReadStream) {
-  var putBar = function () {
-        db.put('bar', 'barvalue', { ttl: 250 })
-        return Date.now()
-      }
-    , verify = function (base, delay) {
+  var retest = function (delay, cb) {
         setTimeout(function () {
-          db2arr(createReadStream, t, function (arr) {
-            var ts = base + 37
-              , i  = 0
-            // allow +/- 3ms leeway, allow for processing speed and Node timer inaccuracy
-            for (; i < 6 && arr[3] && arr[3].value; i++) {
-              if (arr[3] && arr[3].value == String(ts))
-                break
-              ts++
-            }
-            contains(t, arr, 'bar', 'barvalue')
+          db.put('bar', 'barvalue', { ttl: 250 })
+          verifyIn(50, createReadStream, t, function (arr) {
             contains(t, arr, 'foo', 'foovalue')
+            contains(t, arr, 'bar', 'barvalue')
             contains(t, arr, /!ttl!x!\d{13}!bar/, 'bar')
             contains(t, arr, '!ttl!bar', /\d{13}/)
+            cb && cb()
           })
-        }, delay)
-      }
-    , retest = function (delay) {
-        setTimeout(function () {
-          var base = putBar()
-          verify(base, 50)
         }, delay)
       }
     , i
 
   db.put('foo', 'foovalue')
-  for (i = 0; i < 200; i += 20)
+  for (i = 0; i < 180; i += 20)
     retest(i)
-  setTimeout(t.end.bind(t), 300)
+  retest(180, t.end.bind(t))
 })
 
 test('prolong entry life with ttl(key, ttl)', function (t, db, createReadStream) {
-  var ttlBar = function () {
-        db.ttl('bar', 250)
-        return Date.now()
-      }
-    , verify = function (base, delay) {
+  var retest = function (delay, cb) {
         setTimeout(function () {
-          db2arr(createReadStream, t, function (arr) {
-            var ts = base + 37
-              , i  = 0
-            // allow +/- 3ms leeway, allow for processing speed and Node timer inaccuracy
-            for (; i < 6 && arr[3] && arr[3].value; i++) {
-              if (arr[3] && arr[3].value == String(ts))
-                break
-              ts++
-            }
+          db.ttl('bar', 250)
+          verifyIn(25, createReadStream, t, function (arr) {
             contains(t, arr, 'bar', 'barvalue')
             contains(t, arr, 'foo', 'foovalue')
             contains(t, arr, /!ttl!x!\d{13}!bar/, 'bar')
             contains(t, arr, '!ttl!bar', /\d{13}/)
+            cb && cb()
           })
-        }, delay)
-      }
-    , retest = function (delay) {
-        setTimeout(function () {
-          var base = ttlBar()
-          verify(base, 25)
         }, delay)
       }
     , i
 
   db.put('foo', 'foovalue')
   db.put('bar', 'barvalue')
-  for (i = 0; i < 200; i += 20)
+  for (i = 0; i < 180; i += 20)
     retest(i)
-  setTimeout(t.end.bind(t), 300)
+  retest(180, t.end.bind(t))
 })
 
 test('del removes both key and its ttl meta data', function (t, db, createReadStream) {
-  var verify = function (base, delay) {
-        setTimeout(function () {
-          db2arr(createReadStream, t, function (arr) {
-            if (base == -1) {
-              // test complete deletion
-              t.deepEqual(arr, [
-                  { key: 'foo', value: 'foovalue' }
-              ])
-            } else {
-              var ts = base + 197
-                , i  = 0
-              // allow +/- 3ms leeway, allow for processing speed and Node timer inaccuracy
-              for (; i < 6 && arr[3] && arr[3].value; i++) {
-                if (arr[3] && arr[3].value == String(ts))
-                  break
-                ts++
-              }
-              contains(t, arr, 'foo', 'foovalue')
-              contains(t, arr, 'bar', 'barvalue')
-              contains(t, arr, /!ttl!x!\d{13}!bar/, 'bar')
-              contains(t, arr, '!ttl!bar', /\d{13}/)
-            }
-          })
-        }, delay)
-      }
-    , base
-
   db.put('foo', 'foovalue')
-  base = Date.now()
   db.put('bar', 'barvalue', { ttl: 250 })
-  verify(base, 150)
+
+  verifyIn(150, createReadStream, t, function (arr) {
+    contains(t, arr, 'foo', 'foovalue')
+    contains(t, arr, 'bar', 'barvalue')
+    contains(t, arr, /!ttl!x!\d{13}!bar/, 'bar')
+    contains(t, arr, '!ttl!bar', /\d{13}/)
+  })
+
   setTimeout(function () {
     db.del('bar')
   }, 250)
-  // should not exist at all by 70
-  verify(-1, 350)
-  setTimeout(t.end.bind(t), 550)
+
+  verifyIn(350, createReadStream, t, function (arr) {
+    t.deepEqual(arr, [
+      { key: 'foo', value: 'foovalue' }
+    ])
+    t.end()
+  })
 })
 
 test('del removes both key and its ttl meta data (value encoding)', function (t, db, createReadStream) {
-  var verify = function (base, delay) {
-        setTimeout(function () {
-          db2arr(createReadStream, t, function (arr) {
-            if (base == -1) {
-              // test complete deletion
-              t.deepEqual(arr, [
-                  { key: 'foo', value: '{"v":"foovalue"}' }
-              ])
-            } else {
-              var ts = base + 197
-                , i  = 0
-              // allow +/- 10ms leeway, allow for processing speed and Node timer inaccuracy
-              for (; i < 10 && arr[3] && arr[3].value; i++) {
-                if (arr[3] && arr[3].value == String(ts))
-                  break
-                ts++
-              }
-              contains(t, arr, 'foo', '{"v":"foovalue"}')
-              contains(t, arr, 'bar', '{"v":"barvalue"}')
-              contains(t, arr, /!ttl!x!\d{13}!bar/, 'bar')
-              contains(t, arr, '!ttl!bar', /\d{13}/)
-            }
-          }, { valueEncoding: 'utf8' })
-        }, delay)
-      }
-    , base
-
   db.put( 'foo', { v: 'foovalue' })
-  base = Date.now()
   db.put('bar', { v: 'barvalue' }, { ttl: 250 })
-  verify(base, 50)
+
+  verifyIn(50, createReadStream, t, function (arr) {
+    contains(t, arr, 'foo', '{"v":"foovalue"}')
+    contains(t, arr, 'bar', '{"v":"barvalue"}')
+    contains(t, arr, /!ttl!x!\d{13}!bar/, 'bar')
+    contains(t, arr, '!ttl!bar', /\d{13}/)
+  }, { valueEncoding: 'utf8' })
+
   setTimeout(function () {
     db.del('bar')
   }, 175)
-  // should not exist at all by 70
-  verify(-1, 350)
-  setTimeout(t.end.bind(t), 550)
+
+  verifyIn(350, createReadStream, t, function (arr) {
+    t.deepEqual(arr, [
+      { key: 'foo', value: '{"v":"foovalue"}' }
+    ])
+    t.end()
+  }, { valueEncoding: 'utf8' })
+
 }, { keyEncoding: 'utf8', valueEncoding: 'json' })
 
 function wrappedTest () {
@@ -529,11 +454,9 @@ ltest('that level-sublevel data expires properly', function (t, db, createReadSt
 
   ttldb.batch(randomPutBatch(50), { ttl: 100 }, function (err) {
     t.ok(!err, 'no error')
-    setTimeout(function () {
-      db2arr(createReadStream, t, function (arr) {
-        t.equal(arr.length, 0, 'should be empty array')
-        t.end()
-      })
-    }, 150)
+    verifyIn(200, createReadStream, t, function (arr) {
+      t.equal(arr.length, 0, 'should be empty array')
+      t.end()
+    })
   })
 })
